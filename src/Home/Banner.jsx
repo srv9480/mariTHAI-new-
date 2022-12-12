8d50486fbb60d9ddee0a6dc15c2850ae1ccf5b19
srv9480 <102672439+srv9480@users.noreply.github.com>
2022-12-12 22:49:43 +0300

    FIX Кнопка на главной набирает номер, исправлен адрес салона в разделе контакты

--- src/Home/Banner.jsx
+++ src/Home/Banner.jsx
@@ -5,40 +5,45 @@ import ModalWindow from '../components/Modal/Modal';
 import '../assets/fonts/Lora/stylesheet.css';
 
 class Banner extends React.PureComponent {
-   static propTypes = {
-     className: PropTypes.string,
-   }
+  static propTypes = {
+    className: PropTypes.string,
+  }
 
-   static defaultProps = {
-     className: 'home-banner',
-   }
+  static defaultProps = {
+    className: 'home-banner',
+  }
 
-   render() {
-     const { className } = this.props;
-     return (
-       <div className={`home-layout-wrapper ${className}`}>
-         <div className="home-layout">
-           <QueueAnim className={`${className}-content-wrapper`} delay={300} ease="easeOutQuart">
-             <h1 key="h2" style={{ color: 'white'}}>
-               {/* Настоящий тайский массаж */}
-               Дорогие гости, мы открылись! 
-             </h1>
-             <h2
-               className="special"
-               key="h2"
-               style={{
-                 color: 'white',  fontWeight: 'normal',
-               }}
-             >
+  render() {
+    const { className } = this.props;
+    return (
+      <div className={`home-layout-wrapper ${className}`}>
+        <div className="home-layout">
+          <QueueAnim className={`${className}-content-wrapper`} delay={300} ease="easeOutQuart">
+            <h1 key="h2" style={{ color: 'white' }}>
+              {/* Настоящий тайский массаж */}
+              Дорогие гости, мы открылись!
+            </h1>
+            <h2
+              className="special"
+              key="h2"
+              style={{
+                color: 'white', fontWeight: 'normal',
+              }}
+            >
               Ощути магию Таиланда в нашем салоне
-             </h2>
-             <h3 key="h2" className="cta-info" style={{ color: 'white', marginTop: '25px' }}>Получи скидку 20% на первую запись</h3>
-             <ModalWindow />
-           </QueueAnim>
-         </div>
-       </div>
-     );
-   }
+            </h2>
+            <h3 key="h2" className="cta-info" style={{ color: 'white', marginTop: '25px' }}>Запишись сейчас и получи скидку 20%</h3>
+            {/* <ModalWindow /> */}
+            <button className="btn-large" type="button"
+            onClick={() => window.location.href = 'tel:79161043736'}
+            >
+              Получить
+            </button>
+          </QueueAnim>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Banner;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 
 // const path = require('path');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = function(webpackConfig) {
     webpackConfig.babel.plugins.push('transform-runtime');
@@ -8,15 +7,7 @@ module.exports = function(webpackConfig) {
       libraryName: 'antd',
       style: 'css'
     },
-     {
-      "presets": ["@babel/preset-es2015"],
-    }
   ]);
-    // module.exports = {
-    //   plugins: [
-    //     new UglifyJSPlugin()
-    //   ]
-    // }
     return webpackConfig;
   };
 
